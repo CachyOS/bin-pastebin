1df6b89d017832496218aa8d1c1f1aa230e925b6
Vladislav Nepogodin <nepogodin.vlad@gmail.com>
2024-10-06 00:36:21 +0400

    🚧 downgrade syntect dep to fix behavior
    syntect 5 is trying to allocate a lot of memory for highlighting

--- src/models/pretty.rs
+++ src/models/pretty.rs
@@ -1,6 +1,8 @@
 use std::fs;
 use std::path::Path;
+use std::io::Cursor;
 
+use anyhow::Result;
 use syntect::highlighting::ThemeSet;
 use syntect::html::highlighted_html_for_string;
 use syntect::parsing::SyntaxSet;
@@ -13,7 +15,7 @@ static THEMES: &[u8] =
 pub fn get_pretty_body(path: &Path, ext: &str) -> std::io::Result<String> {
     let ss: SyntaxSet = syntect::dumps::from_binary(SYNTAXES);
 
-    let mut theme_cursor = std::io::Cursor::new(THEMES);
+    let mut theme_cursor = Cursor::new(THEMES);
     let theme = ThemeSet::load_from_reader(&mut theme_cursor).unwrap();
 
     let content = fs::read_to_string(path)?;
@@ -21,5 +23,5 @@ pub fn get_pretty_body(path: &Path, ext: &str) -> std::io::Result<String> {
         .find_syntax_by_token(ext)
         .unwrap_or_else(|| ss.find_syntax_plain_text());
 
-    Ok(highlighted_html_for_string(&content, &ss, syntax, &theme))
+    Ok(highlighted_html_for_string(&content, &ss, syntax, &theme).expect("Failed to get highlighted html"))
 }
